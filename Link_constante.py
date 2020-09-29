289f535b9c34ac71c9856d48c782732f45980782
Erica Ferreira <erica.ferreira@poli.ufrj.br>
2020-09-28 21:01:46 -0300

    Create Link_constante.py

---
+++ Link_constante.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+url = 'colocar o link da aula aqui/put your class' link here'
+
+
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
+
+webbrowser.get(chrome_path).open(url)
