9a75617979d720a2581f9ac540175ac8b3ca987f
Erica Ferreira <erica.ferreira@poli.ufrj.br>
2020-09-28 21:09:39 -0300

    Update Link_constante.py

--- Link_constante.py
+++ Link_constante.py
@@ -1,9 +1,14 @@
 import webbrowser
+webbrowser.open('link da aula/class link', new=2)
 
-url = 'colocar o link da aula aqui/put your class' link here'
+'''PT-BR
+Esse código é caso as suas aulas de determinada matéria sempre use o mesmo link
+se new=1, uma nova janela se abrirá 
+se new = 2, uma nova aba se abrirá
+'''
+'''ENG
+This code is to be used if your classes of a certain subjet uses a constant link
+if new = 1, a new window will open
+if new = 2, a new tab will open
+'''
 
-
-chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-
-
-webbrowser.get(chrome_path).open(url)
